e36ea9b450faaa03ae41cc2b73dcc0273ce5a2a5
candale <candale.andrei@gmail.com>
2024-09-29 23:27:09 +0300

    Add lights config

--- src/config.py
+++ src/config.py
@@ -6,60 +6,132 @@ from lights import Light
 configuration = {
     "lights": [
         {
-            "id": "dinning",
-            "verbose_name": "Dinning",
-            "relay_no": 0,
+            "id": "pantry_light",
+            "verbose_name": "Pantry Light",
+            "relay_no":  8,
+            # panel analog: 18
             "input_no": None,
-            "indirect": False
+            "indirect": True
         },
         {
-            "id": "kitchen",
-            "verbose_name": "Kitchen",
-            "relay_no": 1,
+            "id": "kitchen_led",
+            "verbose_name": "Litchen Led",
+            "relay_no":  9,
+            # panel analog: 17
             "input_no": None,
-            "indirect": False
+            "indirect": True
         },
         {
-            "id": "main_living",
-            "verbose_name": "Main living",
-            "relay_no": 2,
+            "id": "kitchen_light",
+            "verbose_name": "Kitchen Light",
+            "relay_no": 10,
+            # panel analog: 16
             "input_no": None,
-            "indirect": False
+            "indirect": True
         },
         {
-            "id": "living_square",
-            "verbose_name": "Living square",
-            "relay_no": 3,
+            "id": "stairs_light",
+            "verbose_name": "Stairs Light",
+            "relay_no": 11,
+            # panel analog: 14
             "input_no": None,
-            "indirect": False
+            "indirect": True
         },
         {
-            "id": "hallway",
-            "verbose_name": "Hallway",
-            "relay_no": 4,
+            "id": "living_square_light",
+            "verbose_name": "Living Square Light",
+            "relay_no": 12,
+            # panel analog: 13
             "input_no": None,
-            "indirect": False
+            "indirect": True
         },
         {
-            "id": "living_path_1",
-            "verbose_name": "Living path 1",
-            "relay_no": 5,
+            "id": "living_center_light",
+            "verbose_name": "Living Center Light",
+            "relay_no": 13,
+            # panel analog: 12
             "input_no": None,
-            "indirect": False
+            "indirect": True
         },
         {
-            "id": "living_path_2",
-            "verbose_name": "Living path 2",
-            "relay_no": 6,
+            "id": "terrace_exit_light",
+            "verbose_name": "Terrace Exit Light",
+            "relay_no": 14,
+            # panel analog: 11
             "input_no": None,
-            "indirect": False
+            "indirect": True
         },
         {
-            "id": "terace_exit",
-            "verbose_name": "Terace exit",
-            "relay_no": 7,
+            "id": "exterior_yard_light",
+            "verbose_name": "Exterior Yard Light",
+            "relay_no": 15,
+            # panel analog: 21
             "input_no": None,
-            "indirect": False
+            "indirect": True
+        },
+        {
+            "id": "exterior_road_light",
+            "verbose_name": "Exterior Road Light",
+            "relay_no": 24,
+            # panel analog: 20
+            "input_no": None,
+            "indirect": True
+        },
+        {
+            "id": "hallway_entrance_light",
+            "verbose_name": "Hallway Entrance Light",
+            "relay_no": 25,
+            # panel analog: 19
+            "input_no": None,
+            "indirect": True
+        },
+        {
+            "id": "dinning_light",
+            "verbose_name": "Dinning Light",
+            "relay_no": 26,
+            # panel analog: 10
+            "input_no": None,
+            "indirect": True
+        },
+        {
+            "id": "dinning_way_light",
+            "verbose_name": "Dinning Way Light",
+            "relay_no": 27,
+            # panel analog: 9
+            "input_no": None,
+            "indirect": True
+        },
+        {
+            "id": "main_way_light",
+            "verbose_name": "Main Way Light",
+            "relay_no": 28,
+            # panel analog: 8
+            "input_no": None,
+            "indirect": True
+        },
+        {
+            "id": "bedroom_way_light",
+            "verbose_name": "Bedroom Way Light",
+            "relay_no": 29,
+            # panel analog: 7
+            "input_no": None,
+            "indirect": True
+        },
+        {
+            "id": "dressing_light",
+            "verbose_name": "Dressing Light",
+            "relay_no": 30,
+            # panel analog: 5
+            "input_no": None,
+            "indirect": True
+        },
+        {
+            "id": "bathroom_shower_light",
+            "verbose_name": "Bathroom Shower Light",
+            "relay_no": 31,
+            # panel analog: 4
+            "input_no": None,
+            "indirect": True
         }
     ]
 }
