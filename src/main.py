7bf2f360ee4b34e0618b0bf1375afb2a99e5a5f7
candale <candale.andrei@gmail.com>
2024-10-09 22:53:30 +0300

    Add connect and disconnect callbacks

--- src/main.py
+++ src/main.py
@@ -1,12 +1,28 @@
+from logging import getLogger
 import paho.mqtt.client as mqtt
 from managers import HassMqttLighstManager
 from config import load_lights
 
 
+logger = getLogger(__name__)
+
+
+def on_connect(client, userdata, flags, rc):
+    logger.info('Connected to mqtt')
+
+
+def on_disconnect(client, userdata, rc):
+    if rc != 0:
+        logger.warning('Disconnected from Broker. Reconnecting...')
+
 
 client = mqtt.Client()
 client.username_pw_set('test', 'password')
 client.connect('localhost', 1883, 60)
+
+client.on_connect = on_connect
+client.on_disconnect = on_disconnect
+
 client.loop_start()
 
 lights = load_lights()
